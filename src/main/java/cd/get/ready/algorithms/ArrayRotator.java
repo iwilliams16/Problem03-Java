6c133987074c7e162a9619bc2b2612eec48c3216
Isaiah Williams <isaiah@Isaiahs-MacBook-Pro-5.local>
2020-10-03 17:12:49 -0400

    "Problem3 Done"

--- src/main/java/cd/get/ready/algorithms/ArrayRotator.java
+++ src/main/java/cd/get/ready/algorithms/ArrayRotator.java
@@ -2,10 +2,23 @@ package cd.get.ready.algorithms;
 
 public class ArrayRotator {
 
-    public int[] rotateLeft(int[] arr, int n){
 
+    public int[] rotateLeft(int[] arr, int n) {
+        //rotate left the number of times given by int n
+        for(int i  = 0; i  < n;  i++) {
+            arr = shift(arr);
+        }
         return arr;
     }
 
+    private int[] shift(int[] myArray){
+        int size = myArray.length;
+        int tempZero = myArray[0];
 
+        for(int i = 0; i < size-1;  i++){ //loop through the array
+            myArray[i] = myArray[i+1];
+        }
+        myArray[size-1] = tempZero;
+        return myArray;
+    }
 }
